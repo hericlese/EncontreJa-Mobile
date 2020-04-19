f56c541db09852934389b3c4a774861b27d4772e
hericlese <hericlesaraujo@hotmail.comgit config --global user.email hericlesaraujo@hotmail.com>
2020-04-18 21:16:44 -0300

    imagens commit

--- app/src/main/java/com/example/encontreja/MainActivity.java
+++ app/src/main/java/com/example/encontreja/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.encontreja;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.activity_main);
+        toolbar=findViewById(R.id.myToolBar);
+
     }
+
+    //adicionando menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
 }
