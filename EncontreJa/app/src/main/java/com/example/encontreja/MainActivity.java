aca5fbde56ad4123adc5ea15cdac716bb83e0288
hericlese <hericlesaraujo@hotmail.comgit config --global user.email hericlesaraujo@hotmail.com>
2020-05-05 14:23:12 -0300

    Home Mobile

---
+++ EncontreJa/app/src/main/java/com/example/encontreja/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.encontreja;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    NavigationView navigationView;
+    ActionBarDrawerToggle toggle;
+    LinearLayout btnRegistrar;
+
+    @SuppressLint("RestrictedApi")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        drawerLayout= findViewById(R.id.drawer); //pagina toolbar escondida
+        toolbar = findViewById(R.id.toolbar);  // toolbar
+        navigationView = findViewById(R.id.navigationView); //barra de navegação
+        btnRegistrar =findViewById(R.id.card_registrar); //Botão Registrar
+
+
+        setSupportActionBar(toolbar); // declarando ação da barra de navegação
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawerOpen,R.string.drawerClose);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
+    }
+
+    // opções do menu
+
+
+
+
+
+
+}
