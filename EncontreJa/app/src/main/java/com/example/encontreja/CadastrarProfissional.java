1d6c1d0325fd90cd38b53e180da96f9fd493bfbc
hericlese <hericlesaraujo@hotmail.comgit config --global user.email hericlesaraujo@hotmail.com>
2020-05-05 18:08:12 -0300

    Activity Registrar Profissional

--- EncontreJa/app/src/main/java/com/example/encontreja/CadastrarProfissional.java
+++ EncontreJa/app/src/main/java/com/example/encontreja/CadastrarProfissional.java
@@ -1,14 +1,64 @@
 package com.example.encontreja;
 
-import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
+import android.annotation.SuppressLint;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.MenuItem;
+import android.widget.GridLayout;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+        import android.view.View;
+        import androidx.annotation.NonNull;
+        import androidx.appcompat.app.ActionBarDrawerToggle;
+        import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.ViewCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+        import com.google.android.material.navigation.NavigationView;
 
-public class CadastrarProfissional extends AppCompatActivity {
+public class CadastrarProfissional extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    NavigationView navigationView;
+    ActionBarDrawerToggle toggle;
+    GridLayout registrarProfissional;
 
 
+    @SuppressLint({"RestrictedApi", "WrongViewCast"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_profissional);
+        drawerLayout= findViewById(R.id.drawer); //pagina toolbar escondida
+        toolbar = findViewById(R.id.toolbar);  // toolbar
+        navigationView = findViewById(R.id.navigationView); //barra de navegação
+
+
+        setSupportActionBar(toolbar); // declarando ação da barra de navegação
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawerOpen,R.string.drawerClose);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
+    }
+
+    // opções do menu
+
+
+
+
+
+
 }
--- EncontreJa/app/src/main/java/com/example/encontreja/MainActivity.java
+++ EncontreJa/app/src/main/java/com/example/encontreja/MainActivity.java
@@ -1,22 +1,20 @@
 package com.example.encontreja;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 import android.view.View;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
-
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
-
 
     DrawerLayout drawerLayout;
     Toolbar toolbar;
@@ -32,7 +30,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawerLayout= findViewById(R.id.drawer); //pagina toolbar escondida
         toolbar = findViewById(R.id.toolbar);  // toolbar
         navigationView = findViewById(R.id.navigationView); //barra de navegação
-        btnRegistrar =findViewById(R.id.card_registrar); //Botão Registrar
+        LinearLayout btnRegistrar = (LinearLayout) findViewById(R.id.card_registrar); //Botão Registrar
+
 
 
         setSupportActionBar(toolbar); // declarando ação da barra de navegação
@@ -42,6 +41,24 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
+
+
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {   //Btn Registrar
+                // passar os dados para outra View (activity_resultado.xml)
+                // Intent(classe origem, classe destino.class)
+                Intent itCadastroProfissional = new Intent(
+                        MainActivity.this,
+                        CadastrarProfissional.class
+                );
+                // chamar a outra Activity
+                startActivity(itCadastroProfissional);
+            }
+        });
+
+
+
+
 
     }
 
